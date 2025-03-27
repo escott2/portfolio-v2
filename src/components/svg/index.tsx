ae21dff82acb47b2761d36e6b21994a641c677a4
escott2 <etriplett14@gmail.com>
2025-03-27 16:48:05 -0500

    alphabetize export statements

--- src/components/svg/index.tsx
+++ src/components/svg/index.tsx
@@ -1,7 +1,7 @@
-export { default as PlaySVG } from "./PlaySVG";
-export { default as PauseSVG } from "./PauseSVG";
 export { default as ClearSVG } from "./ClearSVG";
 export { default as GitHubSVG } from "./GitHubSVG";
 export { default as HandWaveSVG } from "./HandWaveSVG";
 export { default as InternetSVG } from "./InternetSVG";
+export { default as PauseSVG } from "./PauseSVG";
+export { default as PlaySVG } from "./PlaySVG";
 export { default as SmileySVG } from "./SmileySVG";
